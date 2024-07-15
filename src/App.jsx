0bf2bfcc982032196671e028e49ab5dc4debe4f8
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-07-15 19:57:55 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <div className="overflow-x-hidden">
+      <Hero />
+      <Services/>
+      {/* <WheeToBuy/> */}
+      {/* <AppBanner/> */}
+      {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
